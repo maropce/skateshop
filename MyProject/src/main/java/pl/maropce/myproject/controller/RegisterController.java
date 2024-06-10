97da96ea4c2979631f1853edeee011f81b5e9976
maropce <47991391+maropce@users.noreply.github.com>
2024-06-10 07:22:47 +0200

    Update RegisterController.java

--- MyProject/src/main/java/pl/maropce/myproject/controller/RegisterController.java
+++ MyProject/src/main/java/pl/maropce/myproject/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package pl.maropce.myproject.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
