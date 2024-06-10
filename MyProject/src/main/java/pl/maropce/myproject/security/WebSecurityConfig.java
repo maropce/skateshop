f6c7c0a22337e12b48530e4429fcedcb4d548aa7
maropce <47991391+maropce@users.noreply.github.com>
2024-06-10 09:03:28 +0200

    Update WebSecurityConfig.java

--- MyProject/src/main/java/pl/maropce/myproject/security/WebSecurityConfig.java
+++ MyProject/src/main/java/pl/maropce/myproject/security/WebSecurityConfig.java
@@ -2,20 +2,16 @@ package pl.maropce.myproject.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import pl.maropce.myproject.appUser.AppUser;
-import pl.maropce.myproject.appUser.AppUserService;
-import pl.maropce.myproject.appUser.Role;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.maropce.myproject.appUser.UserDetailsServiceImpl;
 
 @Configuration
